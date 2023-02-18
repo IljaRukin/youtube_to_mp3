41760907d313417a6fb71a7e10ff34b469cadcae
IljaRukin <74410869+IljaRukin@users.noreply.github.com>
2023-02-18 13:11:42 +0100

    add keyboardinterrupt

--- download_audio.py
+++ download_audio.py
@@ -1,22 +1,35 @@
-from pytube import YouTube
 import csv
+import sys
+import os
+from pytube import YouTube
 
 failed = list()
 
-with open('download_links.txt','r',encoding='utf-8') as f:
-	for line in f.readlines():
-		line = line.strip('\n')
-        #download
-		try:
-			print('downloading: ',line)
-			YouTube( line ).streams.filter(only_audio=True)[0].download()
-		except Exception as ex:
-			print('error on: ',line)
-			print(ex)
-			failed.append(line)
-
-with open('download_links.txt','w',encoding='utf-8') as ff:
-	for item in failed:
-		_=ff.write('%s\n' % item);
+try:
+	with open('download_links.txt','r',encoding='utf-8') as f:
+		url_list = f.readlines()
+		numElements = len(url_list)
+		for iter in range(numElements):
+			print('-----'+str(iter+1)+'/'+str(numElements)+'-----')
+			line = url_list.pop()
+			line = line.strip('\n')
+			try:
+				print('downloading: ',line)
+				YouTube( line ).streams.filter(only_audio=True)[0].download()
+			except Exception as ex:
+				print('error on: ',line)
+				print(ex)
+				failed.append(line)
+	with open('download_links.txt','w',encoding='utf-8') as ff:
+		for item in (url_list+failed):
+			item = item.strip('\n')
+			_=ff.write('%s\n' % item);
+except KeyboardInterrupt:
+	print('Interrupted')
+	with open('download_links.txt','w',encoding='utf-8') as ff:
+		for item in (url_list+[line]+failed):
+			item = item.strip('\n')
+			_=ff.write('%s\n' % item);
+	sys.exit(0)
 
 print('done \n\n')
